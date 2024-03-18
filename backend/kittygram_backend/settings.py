7856fd762beca909090265764c4581d5a8d07197
Oleg_Bagnii <prostoqisvppppp@gmail.com>
2024-03-18 22:23:47 +0300

    dsfhg

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = os.getenv('DEBUG') == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = ['84.201.161.100', '127.0.0.1', 'localhost', 'kitygramm.zapto.org']
 
